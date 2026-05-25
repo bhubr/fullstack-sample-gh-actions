ca32063e9447e8ad961ad74c05acf8625aaa7d6f
Benoît Hubert <benoithubert@gmail.com>
2026-05-25 13:13:35 +0200

    Assign undeclared var

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 let dummy;
 const moreDummy = 'x';
 moreDummy = 'y';
+evenMoreDummy = 'I was not declared';
 const app = express();
 
 app.get("/message", (req, res) => {
